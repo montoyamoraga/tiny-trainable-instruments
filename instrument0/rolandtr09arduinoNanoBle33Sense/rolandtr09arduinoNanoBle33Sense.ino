8cfe468375d348887a1351073caf33ac975fba92
montoyamoraga <montoyamoraga@gmail.com>
2020-11-19 03:18:50 -0500

    add comments

--- instrument0/rolandtr09arduinoNanoBle33Sense/rolandtr09arduinoNanoBle33Sense.ino
+++ instrument0/rolandtr09arduinoNanoBle33Sense/rolandtr09arduinoNanoBle33Sense.ino
@@ -1,6 +1,22 @@
+// instrument0
+// v0.0.1
+// november 2020
+
+// code by aaron montoya-moraga
+// for thesis tiny trainable instruments
+// with assistance by peter tone
+
+// technology
+// programmed using arduino ide 1.8.13
+// on a macbook air with macos catalina 10.15.7
+// for an arduino nano 33 ble sense
+
+// MIDI notes
+int drumNotes[3] = {37, 39, 42};
 
 void setup() {
-  Serial.begin(9600);
+
+  // start serial communication on pins A0 and A1
   Serial1.begin(9600);
 
   uint32_t baudrate = 0x800000;
@@ -25,5 +41,4 @@ void midiCommand(byte cmd, byte data1, byte data2) {
   Serial1.write(data1);
   Serial1.write(data2);
 
-  Serial.println("beep");
 }
