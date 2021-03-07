fa39a302b4bd5138522ce33e67720abec2a5dd16
montoyamoraga <montoyamoraga@gmail.com>
2021-03-07 17:39:25 -0500

    new PDFs

--- assets/markdown-to-pdf.sh
+++ assets/markdown-to-pdf.sh
@@ -4,6 +4,8 @@ find "$PWD/../docs/md" -iname "*.md" | while read f
 do
   # retrieve basename
   base=$(basename "$f" .md)
-  # pandoc for md to pdf
+  # delete original pdf dile
+  rm -f "$PWD/../docs/pdf/$base.pdf"
+  # create new pdf file with pandoc for md to pdf
   pandoc -s -o "$PWD/../docs/pdf/$base.pdf" "$f"
 done
