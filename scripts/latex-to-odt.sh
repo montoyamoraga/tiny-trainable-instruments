7600bad8fe53097a8e89b630a95d33aad2af0fa7
montoyamoraga <montoyamoraga@gmail.com>
2021-07-07 12:51:21 -0400

    this version works

--- scripts/latex-to-odt.sh
+++ scripts/latex-to-odt.sh
@@ -1,19 +1,12 @@
 echo "latex odt with pandoc"
 
-# iterate through all .odt files in thesis/dot
-find "$PWD/../thesis" -iname "*.odt" | while read f
-do
-  # delete original odt file
-  rm "$f"
-done
-
 # iterate through all .tex files in thesis/
 find "$PWD/../thesis" -iname "*.tex" | while read f
 do
   # retrieve basename
   base=$(basename "$f" .tex)
-  # delete original pdf dile
-  rm -f "$PWD/../thesis/$base.odt"
+  # delete original odt dile
+  rm -f "$PWD/../thesis/odt/$base.odt"
   # create new odt file with pandoc for odt to pdf
-  pandoc -s -o "$PWD/../thesis/$base.odt" "$f"
+  pandoc -s -o "$PWD/../thesis/odt/$base.odt" "$f"
 done
