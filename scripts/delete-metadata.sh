d5fd2ddf3f3080b32b51c047b36ab3d85bc763d0
montoyamoraga <montoyamoraga@gmail.com>
2021-07-07 15:58:02 -0400

    add thesis folder

--- scripts/delete-metadata.sh
+++ scripts/delete-metadata.sh
@@ -1,14 +1,22 @@
 echo "deleting metadata from images with exiftool"
 
-# docs folder
+# certification/
+find "$PWD/../certification/images" -iname "*.jpg" | while read f
+do
+  exiftool -all= -overwrite_original "$f"
+  echo "formatted $f"
+done
+
+
+# docs/
 find "$PWD/../docs/images" -iname "*.jpg" | while read f
 do
   exiftool -all= -overwrite_original "$f"
   echo "formatted $f"
 done
 
-# certification folder
-find "$PWD/../certification/images" -iname "*.jpg" | while read f
+# thesis/
+find "$PWD/../thesis/images" -iname "*.jpg" | while read f
 do
   exiftool -all= -overwrite_original "$f"
   echo "formatted $f"
