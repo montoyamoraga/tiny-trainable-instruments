35b945ff041f6e08fe5a15102055fd7ce4c8ecd1
aarón montoya-moraga <montoyamoraga@gmail.com>
2021-07-12 12:46:34 -0400

    Delete compile-code.sh

--- scripts/compile-code.sh
+++
@@ -1,8 +0,0 @@
-echo "compiling code with arduino-cli"
-arduino-cli core update-index
-arduino-cli board list
-find "$PWD/../TinyTrainable/examples/hello" -type d | while read f
-do  
-  arduino-cli compile --fqbn arduino:mbed:nano33ble "$f"
-  echo "compiled $f with arduino"
-done
