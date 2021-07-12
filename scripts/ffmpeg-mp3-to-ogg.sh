ae6d5a0c893f75d7a6d7f17f10171f4e216810c4
montoyamoraga <montoyamoraga@gmail.com>
2021-07-12 12:56:53 -0400

    add comments

--- scripts/ffmpeg-mp3-to-ogg.sh
+++ scripts/ffmpeg-mp3-to-ogg.sh
@@ -1,6 +1,15 @@
-for i in selection/*.mp3;
+# iterate over every .mp3 in files/
+for i in files/*.mp3;
+  # pipe the filename into cut
+  # -d is delimiter of '.'
+  # -f is the field number, indexed in 1
+  # this assigns to name the filename without the extension
   do name=`echo "$i" | cut -d'.' -f1`
+  # print name
   echo "$name"
+  # ffmpeg input original mp3 and convert to .ogg
   ffmpeg -i "$i" "${name}.ogg"
+  # delete original .mp3 file
   rm "$i"
+# finish iteration
 done
