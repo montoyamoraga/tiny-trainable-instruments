efeba6ef95a9852b0050d8d7efc91f9466fbbf57
montoyamoraga <montoyamoraga@gmail.com>
2021-07-23 10:56:24 -0400

    add hardcoded line for transpiling all chapters to one docx file

--- scripts/pandoc-convert.sh
+++ scripts/pandoc-convert.sh
@@ -2,7 +2,7 @@ echo "pandoc latex to docx"
 
 cd "$PWD/../thesis"
 
-# # iterate through all .tex files in thesis/
+# iterate through all .tex files in thesis/
 find "$PWD" -iname "*.tex" | while read f
 do
   # retrieve basename
@@ -12,3 +12,6 @@ do
   # create new docx file with pandoc
   pandoc -s -o "$PWD/docx/$base.docx" "$f"
 done
+
+# do all the files manually
+pandoc -o "$PWD/docx/aaron-thesis.docx" "$PWD/chap1.tex" "$PWD/chap2.tex" "$PWD/chap3.tex" "$PWD/chap4.tex" "$PWD/chap5.tex" "$PWD/chap6.tex" "$PWD/appa.tex" "$PWD/appb.tex"
